935d571aaea0430aee207b5afab3a4e416b16d06
Henry Shum <henry@metaseit.com>
2016-11-19 20:33:11 +0800

    global layout unit test in progress

--- test/test_fuseboxy_layout.php
+++ test/test_fuseboxy_layout.php
@@ -90,6 +90,9 @@ class TestFuseboxyGlobalLayout extends UnitTestCase {
 
 	function test__layout__breadcrumb() {
 		global $fusebox;
+		Framework::createAPIObject();
+		Framework::loadDefaultConfig();
+		Framework::setMyself();
 		// empty breadcrumb (still have home)
 		$arguments['breadcrumb'] = array();
 		ob_start();
@@ -127,22 +130,224 @@ class TestFuseboxyGlobalLayout extends UnitTestCase {
 
 
 	function test__layout__flash() {
+		global $fusebox;
+		Framework::createAPIObject();
+		Framework::loadDefaultConfig();
+		Framework::setMyself();
+		// flash by session variable
+		// ===> session variable should be cleared
+		$_SESSION['flash'] = 'This is a flash message';
+		ob_start();
+		include dirname(dirname(__FILE__)).'/app/view/global/layout.flash.php';
+		$output = ob_get_clean();
+		$doc = phpQuery::newDocument($output);
+		$this->assertNoPattern('/PHP ERROR/i', $output);
+		$this->assertPattern('/this is a flash message/i', $output);
+		$this->assertTrue( !isset($_SESSION['flash']) );
+		$this->assertTrue( pq('#flash')->length );
+		// flash by local variable
+		$arguments['flash'] = 'This is another flash message';
+		ob_start();
+		include dirname(dirname(__FILE__)).'/app/view/global/layout.flash.php';
+		$output = ob_get_clean();
+		$this->assertNoPattern('/PHP ERROR/i', $output);
+		$this->assertPattern('/this is another flash message/i', $output);
+		// session should be over local variable
+		$_SESSION['flash'] = 'Message at session';
+		$arguments['flash'] = 'Message at local';
+		ob_start();
+		include dirname(dirname(__FILE__)).'/app/view/global/layout.flash.php';
+		$output = ob_get_clean();
+		$this->assertNoPattern('/PHP ERROR/i', $output);
+		$this->assertPattern('/message at session/i', $output);
+		$this->assertNoPattern('/message at local/i', $output);
+		// simple text should be default as warning style
+		$arguments['flash'] = 'This is simple text message';
+		ob_start();
+		include dirname(dirname(__FILE__)).'/app/view/global/layout.flash.php';
+		$output = ob_get_clean();
+		$doc = phpQuery::newDocument($output);
+		$this->assertNoPattern('/PHP ERROR/i', $output);
+		$this->assertPattern('/this is simple text message/i', $output);
+		$this->assertTrue( pq('#flash.alert-warning')->length );
+		// with type defined
+		$arguments['flash'] = array(
+			'type' => 'danger',
+			'message' => 'This is dangerous message',
+		);
+		ob_start();
+		include dirname(dirname(__FILE__)).'/app/view/global/layout.flash.php';
+		$output = ob_get_clean();
+		$doc = phpQuery::newDocument($output);
+		$this->assertNoPattern('/PHP ERROR/i', $output);
+		$this->assertPattern('/this is dangerous message/i', $output);
+		$this->assertTrue( pq('#flash.alert-danger')->length );
+		// with type, icon, title, and message defined
+		$arguments['flash'] = array(
+			'icon' => 'fa fa-info',
+			'type' => 'info',
+			'title' => 'NOTE:',
+			'message' => 'This is informational message',
+		);
+		ob_start();
+		include dirname(dirname(__FILE__)).'/app/view/global/layout.flash.php';
+		$output = ob_get_clean();
+		$doc = phpQuery::newDocument($output);
+		$this->assertNoPattern('/PHP ERROR/i', $output);
+		$this->assertPattern('/this is informational message/i', $output);
+		$this->assertTrue( pq('#flash.alert-info')->length );
+		$this->assertTrue( pq('#flash .fa-info')->length );
+		$text = pq('#flash')->text();
+		$text = preg_replace('/[\r\n]+/', " ", $text);
+		$text = preg_replace('/[ \t]+/', ' ', $text);
+		$text = trim($text);
+		$this->assertPattern('/note: this is informational message/i', $text);
+		// no flash defined
+		if ( isset($arguments['flash']) ) unset($arguments['flash']);
+		if ( isset($_SESSION['flash']) ) unset($_SESSION['flash']);
+		ob_start();
+		include dirname(dirname(__FILE__)).'/app/view/global/layout.flash.php';
+		$output = ob_get_clean();
+		$doc = phpQuery::newDocument($output);
+		$this->assertNoPattern('/PHP ERROR/i', $output);
+		$this->assertFalse( pq('#flash')->length );
+		$this->assertTrue( trim($output) == '' );
+		// clean-up
+		if ( isset($_SESSION['flash']) ) unset($_SESSION['flash']);
+		unset($fusebox);
 	}
 
 
 	function test__layout__footer() {
+		global $fusebox;
+		Framework::createAPIObject();
+		Framework::loadDefaultConfig();
+		Framework::setMyself();
+		// simply make sure something displayed
+		ob_start();
+		include dirname(dirname(__FILE__)).'/app/view/global/layout.footer.php';
+		$output = ob_get_clean();
+		$doc = phpQuery::newDocument($output);
+		$this->assertNoPattern('/PHP ERROR/i', $output);
+		$this->assertTrue( pq('#footer')->length );
+		// clean-up
+		unset($fusebox);
 	}
 
 
 	function test__layout__header() {
+		global $fusebox;
+		Framework::createAPIObject();
+		Framework::loadDefaultConfig();
+		Framework::setMyself();
+		// simply make sure something displayed
+		// ===> no menu
+		ob_start();
+		include dirname(dirname(__FILE__)).'/app/view/global/layout.header.php';
+		$output = ob_get_clean();
+		$doc = phpQuery::newDocument($output);
+		$this->assertNoPattern('/PHP ERROR/i', $output);
+		$this->assertTrue( pq('#header')->length );
+		$this->assertTrue( pq('#nav')->length );
+		$this->assertFalse( pq('.navbar-nav:not(.navbar-right)>li')->length );
+		$this->assertFalse( pq('.navbar-nav.navbar-right>li')->length );
+		// single-level menu
+		$arguments['nav'] = array(
+			array('name' => 'Home', 'url' => F::url('home.index')),
+			array('name' => 'Foo Bar', 'url' => F::url('foo.bar')),
+			array('name' => 'Unit Test', 'url' => F::url('unit.test')),
+		);
+		ob_start();
+		include dirname(dirname(__FILE__)).'/app/view/global/layout.header.php';
+		$output = ob_get_clean();
+		$doc = phpQuery::newDocument($output);
+		$this->assertNoPattern('/PHP ERROR/i', $output);
+		$this->assertPattern('/home/i', pq('.navbar-nav:not(.navbar-right)>li:eq(0)')->text());
+		$this->assertPattern('/foo bar/i', pq('.navbar-nav:not(.navbar-right)>li:eq(1)')->text());
+		$this->assertPattern('/unit test/i', pq('.navbar-nav:not(.navbar-right)>li:eq(2)')->text());
+		$this->assertTrue( pq('.navbar-nav:not(.navbar-right)>li')->length == 3 );
+		$this->assertTrue( pq('.navbar-nav:not(.navbar-right)>li:eq(0)>a')->attr('href') == F::url('home.index') );
+		$this->assertTrue( pq('.navbar-nav:not(.navbar-right)>li:eq(1)>a')->attr('href') == F::url('foo.bar') );
+		$this->assertTrue( pq('.navbar-nav:not(.navbar-right)>li:eq(2)>a')->attr('href') == F::url('unit.test') );
+		unset($arguments['nav']);
+		// double-level menu
+		$arguments['nav'] = array(
+			array('name' => 'People', 'menus' => array(
+				array('name' => 'Staff', 'url' => F::url('people.staff')),
+				array('name' => 'Student', 'url' => F::url('people.student')),
+			)),
+		);
+		ob_start();
+		include dirname(dirname(__FILE__)).'/app/view/global/layout.header.php';
+		$output = ob_get_clean();
+		$doc = phpQuery::newDocument($output);
+		$this->assertNoPattern('/PHP ERROR/i', $output);
+		$this->assertPattern('/people/i', pq('.navbar-nav:not(.navbar-right)>li:eq(0)')->text());
+		$this->assertPattern('/staff/i', pq('.navbar-nav:not(.navbar-right)>li li:eq(0)')->text());
+		$this->assertPattern('/student/i', pq('.navbar-nav:not(.navbar-right)>li li:eq(1)')->text());
+		$this->assertTrue( pq('.navbar-nav:not(.navbar-right)>li')->length == 1 );
+		$this->assertTrue( pq('.navbar-nav:not(.navbar-right)>li li')->length == 2 );
+		$this->assertTrue( pq('.navbar-nav:not(.navbar-right)>li li:eq(0)>a')->attr('href') == F::url('people.staff') );
+		$this->assertTrue( pq('.navbar-nav:not(.navbar-right)>li li:eq(1)>a')->attr('href') == F::url('people.student') );
+		unset($arguments['nav']);
+		// right navigation
+		$arguments['navRight'] = array(
+			array('name' => 'foo'),
+			array('name' => 'bar'),
+		);
+		ob_start();
+		include dirname(dirname(__FILE__)).'/app/view/global/layout.header.php';
+		$output = ob_get_clean();
+		$doc = phpQuery::newDocument($output);
+		$this->assertNoPattern('/PHP ERROR/i', $output);
+		$this->assertFalse( pq('.navbar-nav:not(.navbar-right)>li')->length );
+		$this->assertTrue( pq('.navbar-nav.navbar-right>li')->length );
+		$this->assertPattern('/foo/i', pq('.navbar-nav.navbar-right>li:eq(0)')->text());
+		$this->assertPattern('/bar/i', pq('.navbar-nav.navbar-right>li:eq(1)')->text());
+		unset($arguments['navRight']);
+		// check active, class-name, and nav-header
+		$arguments['nav'] = array(
+			array('name' => 'unit', 'url' => F::url('unit'), 'navHeader' => 'Hello World'),
+			array('name' => 'test', 'url' => F::url('test'), 'active' => true, 'className' => 'foo-bar'),
+		);
+		ob_start();
+		include dirname(dirname(__FILE__)).'/app/view/global/layout.header.php';
+		$output = ob_get_clean();
+		$doc = phpQuery::newDocument($output);
+		$this->assertNoPattern('/PHP ERROR/i', $output);
+		$this->assertTrue( pq('li.active')->length );
+		$this->assertTrue( pq('li.foo-bar')->length );
+		$this->assertTrue( pq('li.dropdown-header')->length );
+		$this->assertTrue( trim( pq('li.active')->text() ) == 'test');
+		$this->assertTrue( trim( pq('li.foo-bar')->text() ) == 'test');
+		$this->assertTrue( trim( pq('li.dropdown-header')->text() ) == 'Hello World' );
+		unset($arguments['nav']);
+		// clean-up
+		unset($fusebox);
 	}
 
 
 	function test__layout__pagination() {
+		global $fusebox;
+		Framework::createAPIObject();
+		Framework::loadDefaultConfig();
+		Framework::setMyself();
+		// simply make sure something displayed
+
+		// clean-up
+		unset($fusebox);
 	}
 
 
 	function test__layout__title() {
+		global $fusebox;
+		Framework::createAPIObject();
+		Framework::loadDefaultConfig();
+		Framework::setMyself();
+		// simply make sure something displayed
+
+		// clean-up
+		unset($fusebox);
 	}
 
 
