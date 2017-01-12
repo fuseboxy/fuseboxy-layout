450c5c486b009d08243f4b45c08a9c3adc26fd1a
Henry Shum <henry@metaseit.com>
2017-01-12 10:49:46 +0800

    fix html validation error of the empty modals

--- app/view/global/layout.modal.php
+++ app/view/global/layout.modal.php
@@ -1,44 +1,44 @@
-<div id="global-modal" class="modal fade" data-background="static" data-nocache tabindex="-1" role="dialog" aria-labelledby="global-modal-label" aria-hidden="true">
+<div id="global-modal" class="modal fade" data-background="static" data-nocache tabindex="-1" role="dialog" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
-			<h1 class="text-center"><i class="fa fa-spinner fa-spin"></i></h1>
+			<h1 class="text-center"><i class="fa fa-spinner fa-spin">&nbsp;</i></h1>
 		</div>
 	</div>
 </div>
 
 
-<div id="global-modal-max" class="modal fade" data-background="static" data-nocache tabindex="-1" role="dialog" aria-labelledby="global-modal-max-label" aria-hidden="true">
+<div id="global-modal-max" class="modal fade" data-background="static" data-nocache tabindex="-1" role="dialog" aria-hidden="true">
 	<div class="modal-dialog modal-max">
 		<div class="modal-content">
-			<h1 class="text-center"><i class="fa fa-spinner fa-spin"></i></h1>
+			<h1 class="text-center"><i class="fa fa-spinner fa-spin">&nbsp;</i></h1>
 		</div>
 	</div>
 </div>
 
 
-<div id="global-modal-lg" class="modal fade" data-background="static" data-nocache tabindex="-1" role="dialog" aria-labelledby="global-modal-lg-label" aria-hidden="true">
+<div id="global-modal-lg" class="modal fade" data-background="static" data-nocache tabindex="-1" role="dialog" aria-hidden="true">
 	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
-			<h1 class="text-center"><i class="fa fa-spinner fa-spin"></i></h1>
+			<h1 class="text-center"><i class="fa fa-spinner fa-spin">&nbsp;</i></h1>
 		</div>
 	</div>
 </div>
 
 
-<div id="global-modal-sm" class="modal fade" data-background="static" data-nocache tabindex="-1" role="dialog" aria-labelledby="global-modal-sm-label" aria-hidden="true">
+<div id="global-modal-sm" class="modal fade" data-background="static" data-nocache tabindex="-1" role="dialog" aria-hidden="true">
 	<div class="modal-dialog modal-sm">
 		<div class="modal-content">
-			<h1 class="text-center"><i class="fa fa-spinner fa-spin"></i></h1>
+			<h1 class="text-center"><i class="fa fa-spinner fa-spin">&nbsp;</i></h1>
 		</div>
 	</div>
 </div>
 
 
-<div id="global-modal-iframe" class="modal fade" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="gobal-modal-iframe-label" aria-hidden="true">
+<div id="global-modal-iframe" class="modal fade" data-backdrop="static" tabindex="-1" role="dialog" aria-hidden="true">
 	<div class="modal-dialog modal-max">
 		<div class="modal-content">
 			<div class="modal-body" style="border-top-left-radius: 5px; border-top-right-radius: 5px; height: 750px; overflow: hidden; padding: 0;">
-				<iframe name="global-modal-iframe" src="" frameborder="0" height="100%" width="100%"></iframe>
+				<iframe name="global-modal-iframe" src="about:blank" style="border: none; height: 100%; width: 100%;"></iframe>
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -48,7 +48,7 @@
 </div>
 
 
-<div id="global-modal-title-iframe" class="modal fade" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="gobal-modal-title-iframe-label" aria-hidden="true">
+<div id="global-modal-title-iframe" class="modal fade" data-backdrop="static" tabindex="-1" role="dialog" aria-hidden="true">
 	<div class="modal-dialog modal-max">
 		<div class="modal-content">
 			<div class="modal-header">
@@ -58,7 +58,7 @@
 				<h4 class="modal-title">&nbsp;</h4>
 			</div>
 			<div class="modal-body" style="height: 700px; padding: 0;">
-				<iframe name="global-modal-title-iframe" src="" frameborder="0" height="100%" width="100%"></iframe>
+				<iframe name="global-modal-title-iframe" src="about:blank" style="border: none; height: 100%; width: 100%;"></iframe>
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
