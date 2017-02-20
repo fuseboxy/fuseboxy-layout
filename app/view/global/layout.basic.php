dc2f8d41ee7937bc334bfbd8e6cf680e9b425af8
Henry Shum <henry@metaseit.com>
2017-02-20 10:35:38 +0800

    remove author meta tag

--- app/view/global/layout.basic.php
+++ app/view/global/layout.basic.php
@@ -8,7 +8,6 @@
 	<meta charset="utf-8" />
 	<title><?php if ( isset($layout['metaTitle']) ) echo $layout['metaTitle']; ?></title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<meta name="author" content="powered by METASEIT (henry@metaseit.com)" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]><script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script><![endif]-->
