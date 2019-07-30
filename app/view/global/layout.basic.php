50c44eb59f26584603b9007fb8e489148808cbdd
Henry Shum <henry.shum@cityu.edu.hk>
2019-07-30 18:29:26 +0800

    remove ie hack, html5shiv, and respond.js

--- app/view/global/layout.basic.php
+++ app/view/global/layout.basic.php
@@ -1,17 +1,10 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="ie ie6"> <![endif]-->
-<!--[if IE 7]>         <html class="ie ie7"> <![endif]-->
-<!--[if IE 8]>         <html class="ie ie8"> <![endif]-->
-<!--[if IE 9]>         <html class="ie ie9"> <![endif]-->
-<!--[if gt IE 9]><!--> <html> <!--<![endif]-->
+<html>
 <head>
 	<meta charset="utf-8" />
 	<title><?php if ( isset($layout['metaTitle']) ) echo $layout['metaTitle']; ?></title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]><script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script><![endif]-->
-	<!--[if lt IE 9]><script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
 	<!-- style -->
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
