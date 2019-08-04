714ff98ab8f982e4c59a4d21d2706cdc45589017
HENRY SHUM <henrygotmojo@gmail.com>
2019-08-04 17:50:56 +0800

    include bootstrap extend in global layout

--- app/view/global/layout.basic.php
+++ app/view/global/layout.basic.php
@@ -8,10 +8,12 @@
 	<!-- style -->
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link href="<?php echo $fusebox->config['baseUrl']; ?>lib/bootstrap-extend/3.1/bootstrap.extend.css" rel="stylesheet" />
 	<link href="<?php echo $fusebox->config['baseUrl']; ?>css/main.css" rel="stylesheet" />
 	<!-- script -->
 	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
+	<script src="<?php echo $fusebox->config['baseUrl']; ?>lib/bootstrap-extend/3.1/bootstrap.extend.js"></script>
 	<script src="<?php echo $fusebox->config['baseUrl']; ?>js/main.js"></script>
 </head>
 <body data-controller="<?php echo $fusebox->controller; ?>" data-action="<?php echo $fusebox->action; ?>" data-ajax-error="modal">
