5b6749e17ee25376c4a22d8d64c381f48b6c97a8
Henry Shum <henry.shum@cityu.edu.hk>
2019-08-09 18:13:29 +0800

    add aria-current to breadcrumb current page

--- app/view/global/layout.breadcrumb.php
+++ app/view/global/layout.breadcrumb.php
@@ -16,7 +16,7 @@
 			<li class="breadcrumb-item"><a href="<?php echo F::url(); ?>"><i class="fa fa-home"></i></a></li>
 			<?php foreach ( $arguments['breadcrumb'] as $key => $val ) : ?>
 				<?php if ( is_numeric($key) ) : ?>
-					<li class="breadcrumb-item active"><?php echo $val; ?></li>
+					<li class="breadcrumb-item active" aria-current="page"><?php echo $val; ?></li>
 				<?php else : ?>
 					<li class="breadcrumb-item"><a href="<?php echo $val; ?>"><?php echo $key; ?></a></li>
 				<?php endif; ?>
