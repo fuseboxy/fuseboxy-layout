992e43a7693022eee173ca01bf9c1de83d7ef65a
HENRY SHUM <henrygotmojo@gmail.com>
2019-08-11 13:43:57 +0800

    deprecate layout-subtitle and apply array format layout-title

--- app/view/global/layout.title.php
+++ app/view/global/layout.title.php
@@ -4,18 +4,24 @@
 		<in>
 			<structure name="$layout">
 				<string name="title" optional="yes" />
-				<string name="subTitle" optional="yes" />
+				<array name="title" optioal="yes">
+					<string name="+" />
+				</array>
 			</structure>
 		</in>
 		<out />
 	</io>
 </fusedoc>
-*/ ?>
+*/
+?>
 <?php if ( !empty($layout['title']) ) : ?>
-	<h1 class="page-header">
-		<?php echo $layout['title']; ?>
-		<?php if ( !empty($layout['subTitle']) ) : ?>
-			<small><?php echo $layout['subTitle']; ?></small>
-		<?php endif; ?>
-	</h1>
+	<?php if ( is_string($layout['title']) ) $layout['title'] = array($layout['title']); ?>
+	<h1 class="page-header"><?php
+		foreach ( $layout['title'] as $level => $title ) :
+			for ( $i=0; $i<$level; $i++ ) :
+				$title = " <small>{$title}</small>";
+			endfor;
+			echo $title;
+		endforeach;
+	?></h1>
 <?php endif; ?>
