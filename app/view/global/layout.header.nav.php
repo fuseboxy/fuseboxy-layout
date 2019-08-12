cac1cb9dd45c656bd46c2dfbbdb5763bbb952f63
Henry Shum <henry.shum@cityu.edu.hk>
2019-08-12 14:01:18 +0800

    apply more attributes (class, style, linkClass, linkStyle, disabled) and make divider be array

--- app/view/global/layout.header.nav.php
+++ app/view/global/layout.header.nav.php
@@ -3,17 +3,23 @@
 	<io>
 		<in>
 			<array name="$menus">
-				<structure name="+">
+				<structure name="$item">
 					<string name="name" />
-					<string name="url" optional="yes" />
-					<string name="navHeader" optional="yes" />
+					<string name="remark" optinonal="yes" />
+					<string name="url" optinonal="yes" />
+					<boolean name="active" optional="yes" />
+					<boolean name="disabled" optional="yes" />
 					<boolean name="newWindow" optional="yes" />
-					<array name="menus" optional="yes" comments="same data structure as base" />
-					<boolean name="active" />
-					<list name="divider" optional="yes" delim=",">
-						<string name="before|after" />
-					</list>
-					<string name="className" optional="yes" />
+					<!-- custom styling -->
+					<string name="class" optional="yes" />
+					<string name="style" optional="yes" />
+					<string name="linkClass" optional="yes" />
+					<string name="linkStyle" optional="yes" />
+					<!-- utilities for dropdown -->
+					<string name="navHeader" optional="yes" />
+					<array name="divider">
+						<string name="+" comments="before|after" />
+					</array>
 				</structure>
 			</array>
 			<number name="$level" optional="yes" default="1" />
@@ -24,50 +30,58 @@
 */
 if ( !function_exists('layoutHeaderNav') ) {
 	function layoutHeaderNav($menus, $level=1) {
-		foreach ( $menus as $m ) {
+		foreach ( $menus as $item ) {
+			// fix variables
+			if ( empty($item['divider']) ) {
+				$item['divider'] = array();
+			} elseif ( $item['divider'] === true ) {
+				$item['divider'] = array('after');
+			} elseif ( is_string($item['divider']) ) {
+				$item['divider'] = array($item['divider']);
+			}
 			// header
-			if ( !empty($m['navHeader']) ) :
-				?><li class="dropdown-header h6"><?php echo $m['navHeader']; ?></li><?php 
+			if ( !empty($item['navHeader']) ) :
+				?><li class="dropdown-header h6"><?php echo $item['navHeader']; ?></li><?php 
 			endif;
-			// menu item
+			// nav item
 			$itemClass = array();
-			if ( $level == 1 ) $itemClass[] = 'nav-item';
-			if ( !empty($m['active'])    ) $itemClass[] = 'active';
-			if ( !empty($m['menus'])     ) $itemClass[] = 'dropdown';
-			if ( !empty($m['className']) ) $itemClass[] = $m['className'];
-			?><li class="<?php echo implode(' ', $itemClass); ?>"><?php
-				// divider (before)
-				if ( isset($m['divider']) and stripos($m['divider'], 'before') !== false ) :
-					?><li class="dropdown-divider"></li><?php
-				endif;
-				// menu link
+			if ( $level == 1               ) $itemClass[] = 'nav-item';
+			if ( !empty($item['active'])   ) $itemClass[] = 'active';
+			if ( !empty($item['disabled']) ) $itemClass[] = 'disabled';
+			if ( !empty($item['menus'])    ) $itemClass[] = 'dropdown';
+			if ( !empty($item['class'])    ) $itemClass[] = $item['class'];
+			// divider (before)
+			if ( in_array('before', $item['divider']) ) :
+				?><li class="dropdown-divider"></li><?php
+			endif;
+			// display nav item
+			?><li 
+				class="<?php echo implode(' ', $itemClass); ?>"
+				<?php if ( !empty($item['style']) ) : ?>style="<?php echo $item['style']; ?>" <?php endif; ?>
+			><?php
+				// nav link
 				$linkClass = array();
 				$linkClass[] =  ( $level == 1 ) ? 'nav-link' : 'dropdown-item';
-				if ( !empty($m['menus']) ) $linkClass[] = 'dropdown-toggle';
+				if ( !empty($item['menus']) ) $linkClass[] = 'dropdown-toggle';
 				// default link
-				$m['url'] = isset($m['url']) ? $m['url'] : '#';
-				?><a
-					href="<?php echo $m['url']; ?>"
-					class="<?php echo implode(' ', $linkClass); ?>"
-					<?php if ( !empty($m['menus']) ) : ?>
-						role="button" 
-						data-toggle="dropdown" 
-						aria-haspopup="true" 
-						aria-expanded="false" 
-					<?php endif; ?>
-					<?php if ( !empty($m['newWindow']) ) : ?>
-						target="_blank"
-					<?php endif; ?>
-				><?php if ( !empty($m['name']) ) echo $m['name']; ?></a><?php
+				$item['url'] = isset($item['url']) ? $item['url'] : '#';
+				// display nav link
+				?><a 
+					href="<?php echo $item['url']; ?>" 
+					class="<?php echo implode(' ', $linkClass); ?>" 
+					<?php if ( !empty($item['linkStyle']) ) : ?>style="<?php echo $item['linkStyle']; ?>"<?php endif; ?>
+					<?php if ( !empty($item['newWindow']) ) : ?>target="_blank"<?php endif; ?>
+					<?php if ( !empty($item['menus']) ) : ?>role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" <?php endif; ?>
+				><?php if ( !empty($item['name']) ) echo $item['name']; ?></a><?php
 				// has submenu
-				if ( !empty($m['menus']) ) :
-					?><ul class="dropdown-menu"><?php layoutHeaderNav($m['menus'], $level+1); ?></ul><?php
+				if ( !empty($item['menus']) ) :
+					?><ul class="dropdown-menu"><?php layoutHeaderNav($item['menus'], $level+1); ?></ul><?php
 				endif;
 			?></li><?php
 			// divider (after)
-			if ( isset($m['divider']) and ( stripos($m['divider'], 'after') !== false or $m['divider'] === true ) ) :
+			if ( in_array('after', $item['divider']) ) :
 				?><li class="dropdown-divider"></li><?php
 			endif;
-		} // foreach-menus
+		} // foreach-item
 	} // function
 } // if-function-exists
