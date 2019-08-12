ad3d8622a1e5143f09fc6dcdae6a6411b8a73f94
Henry Shum <henry.shum@cityu.edu.hk>
2019-08-12 16:33:15 +0800

    finish layout example

--- example/app/view/example/layout.php
+++ example/app/view/example/layout.php
@@ -1,13 +1,21 @@
 <?php
 // 3rd-level layout (pill)
 $tabLayout = array(
-	'style' => 'pill',
+	'style' => 'pills',
 	'position' => $fusebox->action,
-	'header' => '<h6>sub-sub-layout</h6>',
 	'nav' => array(
-		array('name' => 'Pill #1', 'url' => '#', 'active' => true),
-		array('name' => 'Pill #2', 'url' => '#'),
-		array('name' => 'Pill #3', 'url' => '#'),
+		array('name' => 'Normal example', 'url' => '#normal-example', 'active' => true),
+		array('name' => 'Dropdown example', 'url' => '#dropdown-example', 'menus' => array(
+			array('name' => 'Menu #1'),
+			array('name' => 'Menu #2'),
+			array('name' => 'Menu #3'),
+		)),
+		array('name' => 'Button example', 'url' => '#button-example', 'class' => 'text-nowrap', 'buttons' => array(
+			array('name' => 'Edit', 'url' => '#edit-button'),
+			array('name' => 'Delete', 'url' => '#delete-button', 'class' => 'btn-danger'),
+		)),
+		array('name' => 'Remark example', 'url' => '#remark-example', 'remark' => 999),
+		array('name' => 'Disabled example', 'url' => '#disabled-example', 'disabled' => true),
 	),
 );
 //  display
@@ -18,23 +26,14 @@ $layout['content'] = ob_get_clean();
 
 // 2nd-level layout (tab)
 $tabLayout = array(
-	'style' => 'pills',
+	'style' => 'tabs',
 	'position' => $fusebox->action,
-	'navWidth' => 3,
-	'header' => '<h4>Sub-layout</h4>',
-	'footer' => '<h6 class="text-muted">This is footer</h6>',
+	'justify' => F::is('*.top,*.bottom'),
 	'nav' => array(
-		array('name' => 'Left', 'url' => F::url('example.left'), 'active' => F::is('*.left')),
-		array('name' => 'Right', 'url' => F::url('example.right'), 'active' => F::is('*.right'), 'remark' => 'this is remark'),
-		array('name' => 'Top', 'url' => F::url('example.top'), 'active' => F::is('*.top'), 'button' => array(
-			'Edit' => '#',
-			'Delete' => '#',
-		)),
-		array('name' => 'Bottom', 'url' => F::url('example.bottom'), 'active' => F::is('*.bottom'), 'menus' => array(
-			array('name' => 'Menu #1'),
-			array('name' => 'Menu #2'),
-			array('name' => 'Menu #3'),
-		)),
+		array('name' => 'Left Tab', 'url' => F::url('example.left'), 'active' => F::is('*.left')),
+		array('name' => 'Right Tab', 'url' => F::url('example.right'), 'active' => F::is('*.right')),
+		array('name' => 'Top Tab', 'url' => F::url('example.top'), 'active' => F::is('*.top')),
+		array('name' => 'Bottom Tab', 'url' => F::url('example.bottom'), 'active' => F::is('*.bottom')),
 	),
 );
 ob_start();
@@ -43,5 +42,5 @@ $layout['content'] = ob_get_clean();
 
 
 // wrap by global layout
-include F::config('appPath').'view/example/global_layout.php';
+include F::config('appPath').'view/global/layout.php';
 
