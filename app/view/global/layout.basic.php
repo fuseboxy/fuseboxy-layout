9120190c01961588da02576ba5b85fa6a76bb766
Henry Shum <henry.shum@cityu.edu.hk>
2019-08-21 17:23:59 +0800

    load font-awesome 5.x

--- app/view/global/layout.basic.php
+++ app/view/global/layout.basic.php
@@ -7,7 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<!-- style -->
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
-	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link href="https://use.fontawesome.com/releases/v5.10.1/css/all.css" rel="stylesheet" />
 	<link href="<?php echo $fusebox->config['baseUrl']; ?>lib/bootstrap-extend/4.0-alpha/bootstrap.extend.css" rel="stylesheet" />
 	<link href="<?php echo $fusebox->config['baseUrl']; ?>css/main.css" rel="stylesheet" />
 	<!-- script -->
