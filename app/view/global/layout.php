128cebf46064e49eb25540359445fe2a56d12bb6
Henry Shum <henry.shum@cityu.edu.hk>
2019-08-21 17:56:29 +0800

    add example script for right-nav user-sim

--- app/view/global/layout.php
+++ app/view/global/layout.php
@@ -17,26 +17,30 @@ $layout['brand'] = ''; // brand name
 	( class_exists('Auth') and Auth::activeUserInRole('SUPER,ADMIN') ) ? array(
 		'name' => '<i class="fa fa-cog"></i>',
 		'menus' => array(
-			array('navHeader' => '<strong>SETTINGS</strong>', 'divider' => 'after'),
+			array('navHeader' => 'SETTINGS', 'divider' => 'after'),
 			array('name' => 'User Management', 'url' => F::url('user'), 'active' => F::is('user.*')),
 			array('name' => 'System Settings', 'url' => F::url('enum'), 'active' => F::is('enum.*')),
 			array('name' => 'Audit Log',      'url' => F::url('log'),  'active' => F::is('log.*')),
 		),
 	) : null,
 	// user-sim
-	array(
-		'name' => '<i class="fa fa-eye-slash"></i>',
-		'menus' => array(
-			array('navHeader' => '<strong>USER SIMULATION</strong>', 'divider' => 'after'),
-			array('name' => 'Staff #1', 'url' => '#'),
-			array('name' => 'Staff #2', 'url' => '#'),
-		),
-	),
+	( class_exists('Sim') and class_exists('Auth') and Auth::userInRole('SUPER') ) ? call_user_func(function(){
+		$simMenus = array( array('navHeader' => 'USER SIMULATION', 'divider' => 'after') );
+		$simUsers = R::find('user', "id != ? AND role != 'SUPER' AND IFNULL(disabled, 0) = 0 ORDER BY username ASC", array(Auth::user('id')));
+		$simUsers = R::findAll('user');
+		foreach ( $simUsers as $u ) $simMenus[] = array('name' => $u->username, 'url' => F::url("auth.start_sim&user_id={$u->id}"));
+		if ( Sim::user() ) $simMenus[] = array('name' => '<i class="fa fa-sign-out-alt"></i> End Sim', 'url' => F::url('auth.end_sim'), 'divider' => 'before');
+		return $simUsers ? array(
+			'name' => '<i class="fa fa-mask"></i>'.( Sim::user() ? ' ' : '' ).Sim::user('username'),
+			'menus' => $simMenus,
+			'active' => Sim::user(),
+		) : null;
+	}) : null,
 	// logout
-	( class_exists('Auth') and Auth::user() ) ? array(
-		'name' => "<img src='//ssl.gstatic.com/accounts/ui/avatar_2x.png' class='rounded' style='height: 32px; width: 32px; margin: -10px 0;' />",
+	( class_exists('Auth') and Auth::user()  ) ? array(
+		'name' => '<i class="fa fa-user"></i>',
 		'menus' => array(
-			array('navHeader' => '<strong>'.Auth::user('username').'</strong>', 'divider' => 'after'),
+			array('navHeader' => Auth::user('username'), 'divider' => 'after'),
 			array('name' => 'Update Profile', 'url' => F::url('account.profile'), 'active' => F::is('account.profile')),
 			array('name' => 'Change Password', 'url' => F::url('account.password'), 'active' => F::is('account.password')),
 			array('name' => '<i class="fa fa-power-off"></i> Sign Out', 'url' => F::url('auth.logout'), 'divider' => 'before'),
