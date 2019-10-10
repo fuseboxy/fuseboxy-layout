f4fc5d7bdfe8ab8f3f8bff6fa815f00720d49afd
HENRY SHUM <henrygotmojo@gmail.com>
2019-10-10 21:34:08 +0800

    revise modal header for bootstrap 4

--- app/view/global/modal.header.php
+++ app/view/global/modal.header.php
@@ -3,16 +3,18 @@
 	<io>
 		<in>
 			<structure name="$layout">
-				<string name="modalID" />
-				<string name="modalTitleSize" optional="yes" />
-				<string name="modalTitle" optional="yes" />
 				<string name="modalHeader" optional="yes" />
-				<array name="modalNav" optional="yes" comments="mutual exclusive to title">
+				<structure name="modalTitle" optional="yes">
+					<string name="title" />
+					<string name="class" optional="yes" default="h5" />
+				</structure> 
+				<array name="modalNav" optional="yes">
 					<structure name="+">
 						<string name="name" />
 						<string name="url" optional="yes" />
 						<boolean name="newWindow" optional="yes" />
-						<string name="className" optional="yes" />
+						<string name="class" optional="yes" />
+						<string name="linkClass" optional="yes" />
 						<boolean name="active" optional="yes" />
 					</structure>
 				</array>
@@ -22,50 +24,64 @@
 	</io>
 </fusedoc>
 */
-$layout['modalTitleSize'] = isset($layout['modalTitleSize']) ? $layout['modalTitleSize'] : 'h4';
-?>
-<!-- modal title & header text -->
-<?php if ( !empty($layout['modalTitle']) or !empty($layout['modalHeader']) ) : ?>
-	<div class="modal-header" <?php if ( !empty($layout['modalNav']) ) : ?>style="border: none; margin-bottom: 0; padding-bottom: 0;"<?php endif; ?>>
-		<!-- close button -->
-		<button type="button" class="close" data-dismiss="modal">
-			<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
-		</button>
-		<!-- title -->
-		<?php if ( !empty($layout['modalTitle']) ) : ?>
-			<<?php echo $layout['modalTitleSize']; ?> class="modal-title">
-				<?php echo $layout['modalTitle']; ?>
-			</<?php echo $layout['modalTitleSize']; ?>>
-		<?php endif; ?>
-		<!-- header -->
-		<?php if ( !empty($layout['modalHeader']) ) : ?>
-			<?php if ( !empty($layout['modalTitle']) ) echo '<br />'; ?>
-			<div><?php echo $layout['modalHeader']; ?></div>
-		<?php endif; ?>
-	</div>
-<?php endif; ?>
+// fix parameter
+if ( isset($layout['modalTitle']) and is_string($layout['modalTitle']) ) {
+	$layout['modalTitle'] = array(
+		'title' => $layout['modalTitle'],
+		'class' => 'h5',
+	);
+}
 
 
-<!-- modal nav -->
-<?php if ( !empty($layout['modalNav']) ) : ?>
-	<ul class="nav modal-header nav-tabs" style="margin-bottom: 0; padding-bottom: 0;">
-		<!-- nav -->
-		<?php foreach ( $layout['modalNav'] as $i => $m ) : ?>
-			<li class="<?php if ( !empty($m['active']) ) echo 'active'; ?> <?php if ( isset($m['className']) ) echo $m['className']; ?>">
-				<a
-					<?php if ( isset($m['url']) ) : ?>href="<?php echo $m['url']; ?>"<?php endif; ?>
-					<?php if ( empty($m['newWindow']) ) : ?>
-						data-target="#<?php echo $layout['modalID']; ?>"
-						data-toggle="ajax-load"
-						data-toggle-transition="none"
-					<?php else : ?>
-						target="_blank"
-					<?php endif; ?>
-				><?php if ( isset($m['name']) ) echo $m['name']; ?></a></li>
-		<?php endforeach; ?>
-		<!-- close button for nav (when necessary) -->
-		<?php if ( empty($layout['modalTitle']) and empty($layout['modalHeader']) ) : ?>
-			<li class="close pull-right" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></li>
-		<?php endif; ?>
-	</ul>
-<?php endif; ?>
+// modal title
+if ( !empty($layout['modalTitle']) ) :
+	?><div class="modal-header"><?php
+		?><div class="modal-title <?php echo $layout['modalTitle']['class']; ?>"><?php echo $layout['modalTitle']['title']; ?></div>
+		?><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	?></div><?php
+endif;
+
+
+// modal header
+if ( !empty($layout['modalHeader']) ) :
+	?><div class="modal-header"><?php
+		echo $layout['modalHeader'];
+		?><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><?php
+	?></div><?php
+endif;
+
+
+// modal nav
+if ( !empty($layout['modalNav']) ) :
+	?><div class="modal-header pb-0"><?php
+		?><ul class="nav nav-tabs" style="margin-bottom: -1px;"><?php
+			foreach ( $layout['modalNav'] as $item ) :
+				// nav item
+				$itemClass = array('nav-item mr-1');
+				if ( !empty($item['class']) ) $itemClass[] = $item['class'];
+				// display item
+				?><li class="<?php echo implode(' ', $itemClass); ?>"><?php
+					// nav link
+					$linkClass = array('nav-link');
+				if ( !empty($item['active']) ) $linkClass[] = 'active';
+					if ( !empty($item['linkClass']) ) $linkClass[] = $item['linkClass'];
+					// display link
+					?><a
+						class="<?php echo implode(' ', $linkClass); ?>"
+						<?php if ( !empty($item['url']) ) : ?>
+							href="<?php echo $item['url']; ?>"
+						<?php endif; ?>
+						<?php if ( !empty($item['url']) and !empty($item['newWindow']) ) : ?>
+							target="_blank"
+						<?php elseif ( !empty($item['url']) ) : ?>
+							data-toggle="ajax-load"
+							data-toggle-transition="none"
+							data-target="[id^=global-modal].modal.show .modal-content"
+						<?php endif; ?>
+					><?php if ( isset($item['name']) ) echo $item['name']; ?></a><?php
+				?></li><?php
+			endforeach;
+		?></ul><!--/.nav-tabs--><?php
+		?><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><?php
+	?></div><!--/.modal-header--><?php
+endif;
