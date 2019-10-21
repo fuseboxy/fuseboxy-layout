ea6686e690e510c4a1d65d6dfbd586fe77ab9978
henrygotmojo <henrygotmojo@gmail.com>
2019-10-21 18:01:23 +0800

    adjust suggested footer padding and content height

--- app/view/global/layout.footer.php
+++ app/view/global/layout.footer.php
@@ -1,4 +1,4 @@
-<footer id="footer">
+<footer id="footer" class="pb-5">
 	<hr />
 	<div class="container-fluid small text-muted text-right">
 		Copyright &copy; <?php echo date('Y'); ?>. All Rights Reserved.
