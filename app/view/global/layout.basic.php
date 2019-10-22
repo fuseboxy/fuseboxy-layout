caa63b7bc2ab8b3a046733da4c6d8444f5347b6a
henrygotmojo <henrygotmojo@gmail.com>
2019-10-22 18:06:58 +0800

    apply bootstrap-extend on cdn

--- app/view/global/layout.basic.php
+++ app/view/global/layout.basic.php
@@ -8,16 +8,17 @@
 	<!-- style -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.1/css/all.css" />
+	<link rel="stylesheet" href="https://bbcdn.githack.com/henrygotmojo/bootstrap-extend/raw/4.0/bootstrap.extend.min.css" />
 	<?php if ( class_exists('Scaffold') ) : ?>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-bs4.css" />
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.min.css" />
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
 	<?php endif; ?>
-	<link rel="stylesheet" href="<?php echo $fusebox->config['baseUrl']; ?>lib/bootstrap-extend/4.0/bootstrap.extend.css" />
 	<link rel="stylesheet" href="<?php echo $fusebox->config['baseUrl']; ?>css/main.css" />
 	<!-- script -->
 	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
+	<script src="https://bbcdn.githack.com/henrygotmojo/bootstrap-extend/raw/4.0/bootstrap.extend.min.js"></script>
 	<?php if ( class_exists('Scaffold') ) : ?>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-bs4.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.full.min.js"></script>
@@ -25,7 +26,6 @@
 		<script src="<?php echo F::config('baseUrl'); ?>lib/simple-ajax-uploader/2.6.7/SimpleAjaxUploader.min.js"></script>
 		<script src="<?php echo F::config('baseUrl'); ?>js/fuseboxy.scaffold.js"></script>
 	<?php endif; ?>
-	<script src="<?php echo $fusebox->config['baseUrl']; ?>lib/bootstrap-extend/4.0/bootstrap.extend.js"></script>
 	<script src="<?php echo $fusebox->config['baseUrl']; ?>js/main.js"></script>
 </head>
 <body data-controller="<?php echo $fusebox->controller; ?>" data-action="<?php echo $fusebox->action; ?>">
