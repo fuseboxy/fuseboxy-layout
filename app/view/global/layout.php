18616404d4ddd770d1f8c68a073e9114dadc6873
henrygotmojo <henrygotmojo@gmail.com>
2020-01-06 12:13:05 +0800

    add comment

--- app/view/global/layout.php
+++ app/view/global/layout.php
@@ -1,14 +1,26 @@
-<?php
+<?php /*
+<fusedoc>
+	<io>
+		<in />
+		<out>
+			<structure name="$layout">
+				<string name="content" />
+			</structure>
+		</out>
+	</io>
+</fusedoc>
+*/
+
 // title & nav
 if ( file_exists(__DIR__.'/layout.settings.php') ) include 'layout.settings.php';
 
 
-// display altogether
+// display
 ob_start();
 include 'layout.body.php';
 include 'layout.modal.php';
 $layout['content'] = ob_get_clean();
 
 
-// wrap by <html> and <body>
+// wrap by HTML & BODY
 include 'layout.basic.php';
