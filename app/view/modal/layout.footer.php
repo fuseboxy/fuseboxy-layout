e7f630bbc1447f358e65bc43ae6e5b947f9eba2b
henrygotmojo <henrygotmojo@gmail.com>
2020-02-04 11:14:41 +0800

    fix display bug in modal footer

--- app/view/modal/layout.footer.php
+++ app/view/modal/layout.footer.php
@@ -19,7 +19,7 @@
 		// execution time
 		if ( isset($GLOBALS['startTick']) ) :
 			$et = round(microtime(true)*1000-$GLOBALS['startTick']);
-			?><div class="small w-100"><small class="text-muted">Execution time: <?php echo $et; ?>ms</small></div><?php
+			?><div class="small mr-auto"><small class="text-muted">Execution time: <?php echo $et; ?>ms</small></div><?php
 		endif;
 		// button
 		?><button type="button" class="btn btn-light" data-dismiss="modal">Close</button><?php
