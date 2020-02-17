964d0d36c20920c88b506a040215fbc0db480d8f
henrygotmojo <henrygotmojo@gmail.com>
2020-02-17 12:52:11 +0800

    offer more settings for layout title

--- app/view/global/layout.title.php
+++ app/view/global/layout.title.php
@@ -3,24 +3,29 @@
 	<io>
 		<in>
 			<structure name="$layout">
+				<structure name="title" optional="yes">
+					<string name="tag" optional="yes" default="h1" />
+					<string name="icon" optional="yes" />
+					<string name="class" optional="yes" default="page-header border-bottom mb-4" />
+					<string name="message" />
+				</structure>
 				<string name="title" optional="yes" />
-				<array name="title" optioal="yes">
-					<string name="+" />
-				</array>
 			</structure>
 		</in>
 		<out />
 	</io>
 </fusedoc>
 */
-if ( !empty($layout['title']) ) :
-	if ( is_string($layout['title']) ) $layout['title'] = array($layout['title']);
-	?><h1 class="page-header border-bottom mb-4"><?php
-		foreach ( $layout['title'] as $level => $title ) :
-			// deeper the level smaller the title
-			for ( $i=0; $i<$level; $i++ ) $title = "<small>{$title}</small>";
-			// display this level of title
-			echo "<span class='mr-3'>{$title}</span>";
-		endforeach;
-	?></h1><?php
+if ( isset($layout['title']) and is_string($layout['title']) ) $layout['title'] = array('message' => $layout['title']);
+
+// default
+if ( !isset($layout['title']['tag']) ) $layout['title']['tag'] = 'h1';
+if ( !isset($layout['title']['class']) ) $layout['title']['class'] = 'page-header border-bottom mb-4';
+
+// display
+if ( !empty($layout['title']['message']) ) :
+	echo "<{$layout['title']['tag']} class='{$layout['title']['class']}'>";
+	if ( !empty($layout['title']['icon']) ) echo "<i class='{$layout['title']['icon']}'></i>";
+	echo $layout['title']['message'];
+	echo "</{$layout['title']['tag']}>";
 endif;
