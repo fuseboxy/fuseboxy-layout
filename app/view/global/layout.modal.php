7892f59a0d6111579ef2f9e2ac55c782bb28eb90
henrygotmojo <henrygotmojo@gmail.com>
2020-03-17 13:08:49 +0800

    fix typo

--- app/view/global/layout.modal.php
+++ app/view/global/layout.modal.php
@@ -1,5 +1,5 @@
 <?php foreach ( ['sm','md','lg','xl','max'] as $size ) : ?>
-	<div id="global-modal-<?php echo $size; ?>" class="modal fade" data-background="static" tabindex="-1" role="dialog" aria-hidden="true">
+	<div id="global-modal-<?php echo $size; ?>" class="modal fade" data-backdrop="static" tabindex="-1" role="dialog" aria-hidden="true">
 		<div class="modal-dialog modal-<?php echo $size; ?>">
 			<div class="modal-content">
 			</div>
