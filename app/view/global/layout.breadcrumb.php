4399408e0019290e6aba1fd4c815a694d1ab1aba
henrygotmojo <henrygotmojo@gmail.com>
2020-09-29 11:06:34 +0800

    skip empty breadcrumb item

--- app/view/global/layout.breadcrumb.php
+++ app/view/global/layout.breadcrumb.php
@@ -13,14 +13,16 @@
 if ( isset($arguments['breadcrumb']) ) :
 	?><nav id="breadcrumb" aria-label="breadcrumb">
 		<ol class="breadcrumb">
-			<li class="breadcrumb-item"><a href="<?php echo F::url(); ?>"><i class="fa fa-home"></i></a></li>
-			<?php foreach ( $arguments['breadcrumb'] as $key => $val ) : ?>
-				<?php if ( is_numeric($key) ) : ?>
-					<li class="breadcrumb-item active" aria-current="page"><?php echo $val; ?></li>
-				<?php else : ?>
-					<li class="breadcrumb-item"><a href="<?php echo $val; ?>"><?php echo $key; ?></a></li>
-				<?php endif; ?>
-			<?php endforeach; ?>
-		</ol>
+			<li class="breadcrumb-item"><a href="<?php echo F::url(); ?>"><i class="fa fa-home"></i></a></li><?php
+			foreach ( $arguments['breadcrumb'] as $key => $val ) :
+				// item with link
+				if ( is_string($key) ) :
+					?><li class="breadcrumb-item"><a href="<?php echo $val; ?>"><?php echo $key; ?></a></li><?php
+				// item without link
+				elseif ( is_numeric($key) and !empty($val) ) :
+					?><li class="breadcrumb-item active" aria-current="page"><?php echo $val; ?></li><?php
+				endif;
+			endforeach;
+		?></ol>
 	</nav><?php
 endif;
