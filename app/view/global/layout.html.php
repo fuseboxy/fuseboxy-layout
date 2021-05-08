a731883d30a9b3007c982ead7fffcee9ecbb4a42
henrygotmojo <henrygotmojo@gmail.com>
2021-05-08 11:22:15 +0800

    load bootstrap icon font

--- app/view/global/layout.html.php
+++ app/view/global/layout.html.php
@@ -7,6 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="<?php echo F::config('baseUrl'); ?>../bootstrap-extend/bootstrap.extend.css" />
 <script src="<?php echo F::config('baseUrl'); ?>../bootstrap-extend/bootstrap.extend.js"></script><?php
