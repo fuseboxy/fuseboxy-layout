0834f4cfdacc02c9681cdd29647e8819ee2be5ae
henrygotmojo <henrygotmojo@gmail.com>
2021-05-08 11:14:58 +0800

    load latest jquery & bootstrap 5.x & bootstrap-extend in dev

--- app/view/global/layout.html.php
+++ app/view/global/layout.html.php
@@ -5,13 +5,11 @@
 	<title><?php if ( isset($layout['metaTitle']) ) echo $layout['metaTitle']; ?></title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
-	<link rel="stylesheet" href="https://cdn.statically.io/bb/henrygotmojo/bootstrap-extend/4.2.1/bootstrap.extend.css" />
-	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-	<script src="https://cdn.statically.io/bb/henrygotmojo/bootstrap-extend/4.2.1/bootstrap.extend.js"></script><?php
-	// scaffold
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous" />
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="<?php echo F::config('baseUrl'); ?>../bootstrap-extend/bootstrap.extend.css" />
+<script src="<?php echo F::config('baseUrl'); ?>../bootstrap-extend/bootstrap.extend.js"></script><?php
 	if ( class_exists('Scaffold') ) :
 		?><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.min.css" />
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
@@ -24,9 +22,7 @@
 		<script src="https://cdn.jsdelivr.net/npm/simple-ajax-uploader@2.6.7/SimpleAjaxUploader.js"></script>
 		<script src="https://cdn.statically.io/bb/henrygotmojo/fuseboxy-scaffold-asset/2.1.3/fuseboxy.scaffold.js"></script><?php
 	endif;
-	// captcha
-	if ( class_exists('Captcha') and F::is('auth.*') ) echo Captcha::api();
-	// custom
+	if ( class_exists('Captcha') and F::is('auth.*') ) : echo Captcha::api();
 	if ( is_file( F::config('baseDir').'css/main.css' ) ) :
 		?><link rel="stylesheet" href="<?php echo F::config('baseUrl'); ?>css/main.css" /><?php
 	endif;
