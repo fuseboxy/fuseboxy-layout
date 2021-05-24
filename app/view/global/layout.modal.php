508c072d031dace12b8b06baca9e17f5c12e6bd9
henrygotmojo <henrygotmojo@gmail.com>
2021-05-24 10:18:01 +0800

    replace modal-max (extend) with modal-fullscreen (official) and always apply modal-dialog-scrollable

--- app/view/global/layout.modal.php
+++ app/view/global/layout.modal.php
@@ -6,9 +6,9 @@
 	<io />
 </fusedoc>
 */
-foreach ( ['max','xl','lg','md','sm','xs'] as $size ) :
+foreach ( ['fullscreen','xl','lg','md','sm','xs'] as $size ) :
 	?><div id="global-modal-<?php echo $size; ?>" class="modal fade" data-backdrop="true" tabindex="-1" role="dialog" aria-hidden="true"><?php
-		?><div class="modal-dialog modal-<?php echo $size; ?>"><?php
+		?><div class="modal-dialog modal-dialog-scrollable modal-<?php echo $size; ?>"><?php
 			?><div class="modal-content"></div><?php
 		?></div><?php
 	?></div><?php
