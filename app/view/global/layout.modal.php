460d79f802173f4ae41f368911aad66d8b23cb40
henrygotmojo <henrygotmojo@gmail.com>
2021-05-25 11:51:51 +0800

    add modal-xxl size (equivalent to modal-max in bootstrap-extend 4)

--- app/view/global/layout.modal.php
+++ app/view/global/layout.modal.php
@@ -6,7 +6,7 @@
 	<io />
 </fusedoc>
 */
-foreach ( ['fullscreen','xl','lg','md','sm','xs'] as $size ) :
+foreach ( ['fullscreen','xxl','xl','lg','md','sm','xs'] as $size ) :
 	?><div id="global-modal-<?php echo $size; ?>" class="modal fade" data-backdrop="true" tabindex="-1" role="dialog" aria-hidden="true"><?php
 		?><div class="modal-dialog modal-dialog-scrollable modal-<?php echo $size; ?>"><?php
 			?><div class="modal-content"></div><?php
