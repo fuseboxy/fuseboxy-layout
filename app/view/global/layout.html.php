75038c195ed738cc584dac07af4c2f6c90db5b05
henrygotmojo <henrygotmojo@gmail.com>
2021-09-03 17:05:38 +0800

    default load jquery-knob for webform

--- app/view/global/layout.html.php
+++ app/view/global/layout.html.php
@@ -26,6 +26,7 @@
 	if ( class_exists('Webform') ) :
 		?><link rel="stylesheet" href="https://cdn.statically.io/bb/henrygotmojo/fuseboxy-webform-asset/0.5.1/fuseboxy.webform.css" />
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jSignature/2.1.3/jSignature.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jQuery-Knob/1.2.13/jquery.knob.min.js"></script>
 		<script src="https://cdn.statically.io/bb/henrygotmojo/fuseboxy-webform-asset/0.5.1/fuseboxy.webform.js"></script><?php
 	endif;
 	// scaffold & webform
