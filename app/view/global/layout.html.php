3955a76ea88b853aa20832679f901b39b0fdb4a6
henrygotmojo <henrygotmojo@gmail.com>
2022-02-24 15:51:52 +0800

    update scaffold asset version

--- app/view/global/layout.html.php
+++ app/view/global/layout.html.php
@@ -17,10 +17,10 @@
 	// scaffold
 	if ( class_exists('Scaffold') ) :
 		?><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" />
-		<link rel="stylesheet" href="https://cdn.statically.io/bb/henrygotmojo/fuseboxy-scaffold-asset/2.1.5/fuseboxy.scaffold.css" />
+		<link rel="stylesheet" href="https://cdn.statically.io/bb/henrygotmojo/fuseboxy-scaffold-asset/2.1.6/fuseboxy.scaffold.css" />
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.18/summernote-bs4.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
-		<script src="https://cdn.statically.io/bb/henrygotmojo/fuseboxy-scaffold-asset/2.1.5/fuseboxy.scaffold.js"></script><?php
+		<script src="https://cdn.statically.io/bb/henrygotmojo/fuseboxy-scaffold-asset/2.1.6/fuseboxy.scaffold.js"></script><?php
 	endif;
 	// webform
 	if ( class_exists('Webform') ) :
