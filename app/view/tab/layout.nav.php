cb028cb00468ea4361c13908b2a274474c15fdc6
henrygotmojo <henrygotmojo@gmail.com>
2022-08-09 12:22:36 +0800

    modify tab nav and allow no link

--- app/view/tab/layout.nav.php
+++ app/view/tab/layout.nav.php
@@ -6,7 +6,7 @@
 				<string name="style" comments="tabs|pills|~empty~" />
 				<string name="position" comments="left|right|top|bottom" />
 				<string name="orientation" comments="vertical|horizontal" />
-				<boolean name="justify" optional="yes" default="false" comments="true|false|fill|center|end" />
+				<boolean name="justify" optional="yes" default="false" comments="~true~|~false~|fill|center|end" />
 				<string name="header" optional="yes" />
 				<string name="headerClass" optional="yes" default="h4 mb-3" />
 				<string name="footer" optional="yes" />
@@ -31,8 +31,6 @@
 						<array name="buttons" optional="yes" />
 						<!-- dropdown -->
 						<array name="menus" optional="yes" />
-						<!-- others -->
-						<string name="output" optional="yes" comments="simply output and do not display link" />
 					</structure>
 				</array>
 			</structure>
@@ -70,73 +68,72 @@ if ( $tabLayout['justify'] === true ) {
 	endif;
 	// nav items
 	if ( !empty($tabLayout['nav']) ) :
-		foreach ( $tabLayout['nav'] as $tabKey => $tab ) :
+		foreach ( $tabLayout['nav'] as $itemKey => $item ) :
 			// display tab (when necessary)
-			if ( !empty($tab) ) :
+			if ( !empty($item) ) :
 				// fix tab (when necessary)
-				if ( is_string($tab) ) $tab = array('output' => $tab);
-				elseif ( !is_numeric($tabKey) and empty($tab['name']) ) $tab['name'] = $tabKey;
-				// menu item
+				if ( is_string($item) ) $item = array('name' => $item);
+				elseif ( !is_numeric($itemKey) and empty($item['name']) ) $item['name'] = $itemKey;
+				// prepare item class
 				$itemClass = array('nav-item');
-				if ( !empty($tab['class']) ) $itemClass[] = $tab['class'];
-				if ( !empty($tab['attr']['class']) ) $itemClass[] = $tab['attr']['class'];
+				if ( !empty($item['class']) ) $itemClass[] = $item['class'];
+				if ( !empty($item['attr']['class']) ) $itemClass[] = $item['attr']['class'];
 				$itemClass[] = ( $tabLayout['orientation'] == 'vertical' ) ? 'mb-1' : 'mr-1';
+				// prepare item attributes
+				$itemAttr = $item['attr'] ?? array();
+				$itemAttr['class'] = implode(' ', $itemClass);
 				// display menu item
-				?><li class="<?php echo implode(' ', $itemClass); ?>"><?php
+				?><li <?php foreach ( $itemAttr as $key => $val ) echo $key.'="'.$val.'" '; ?>><?php
 					// buttons (for vertical orientation)
-					if ( !empty($tab['buttons']) and $tabLayout['orientation'] == 'vertical' ) :
+					if ( !empty($item['buttons']) and $tabLayout['orientation'] == 'vertical' ) :
 						?><div class="float-right"><?php
 							include F::appPath('view/tab/layout.nav.button.php');
 						?></div><?php
 					endif;
-					// dropdown menu
-					if ( !empty($tab['menus']) ) :
-						?><ul class="dropdown-menu"><?php layoutHeaderNav($tab['menus'], 2); ?></ul><?php
-					endif;
-					// display custom attribute
-					if ( !empty($tab['attr']) ) foreach ( $tab['attr'] as $key => $val ) if ( $key != 'class' ) echo $key.'="'.$val.'" ';
-					// link styling
+					// prepare link class
 					$linkClass = array('nav-link');
-					if ( !empty($tab['active']) ) $linkClass[] = 'active';
-					if ( !empty($tab['disabled']) ) $linkClass[] = 'disabled';
-					if ( !empty($tab['linkClass']) ) $linkClass[] = $tab['linkClass'];
-					if ( !empty($tab['linkAttr']['class']) ) $itemClass[] = $tab['linkAttr']['class'];
-					if ( !empty($tab['buttons']) and $tabLayout['orientation'] == 'horizontal' ) $linkClass[] = 'd-inline-block';
-					// display menu link
-					?><a 
-						class="<?php echo implode(' ', $linkClass); ?>"
-						<?php if ( !empty($tab['url']) ) : ?>
-							href="<?php echo $tab['url']; ?>"
-						<?php endif; ?>
-						<?php if ( !empty($tab['newWindow']) ) : ?>
-							target="_blank"
-						<?php endif; ?>
-						<?php if ( !empty($tab['menus']) ) : ?>
-							data-toggle="dropdown"
-							role="button"
-							aria-haspopup="true"
-							aria-expanded="false"
-						<?php endif; ?>
-						<?php if ( !empty($tab['linkAttr']) ) foreach ( $tab['linkAttr'] as $key => $val ) if ( $key != 'class' ) echo $key.'="'.$val.'" '; ?>
-					><?php
-						// tab icon
-						if ( !empty($tab['icon']) ) :
-							?><i class="<?php echo $tab['icon']; ?>"></i> <?php
-						endif;
-						// tab name
-						if ( !empty($tab['name']) ) :
-							$tabNameClass = array();
-							?><span class="<?php echo implode(' ', $tabNameClass); ?>"><?php echo $tab['name']; ?></span><?php
-						endif;
-						// tab remark
-						if ( !empty($tab['remark']) ) :
-							$tabRemarkClass = array('ml-1');
-							if ( $tabLayout['style'] == 'tabs' or empty($tab['active']) ) $tabRemarkClass[] = 'text-muted';
-							?> <small class="<?php echo implode(' ', $tabRemarkClass); ?>">(<?php echo $tab['remark']; ?>)</small><?php
-						endif;
-					?></a><!--/.nav-link--><?php
+					if ( !empty($item['active']) ) $linkClass[] = 'active';
+					if ( !empty($item['disabled']) ) $linkClass[] = 'disabled';
+					if ( !empty($item['linkClass']) ) $linkClass[] = $item['linkClass'];
+					if ( !empty($item['linkAttr']['class']) ) $itemClass[] = $item['linkAttr']['class'];
+					if ( !empty($item['buttons']) and $tabLayout['orientation'] == 'horizontal' ) $linkClass[] = 'd-inline-block';
+					// prepare link attributes
+					$linkAttr = $item['linkAttr'] ?? array();
+					$linkAttr['class'] = implode(' ', $linkClass);
+					if ( !empty($item['url']) ) $linkAttr['href'] = $item['url'];
+					if ( !empty($item['newWindow']) ) $linkAttr['target'] = '_blank';
+					if ( !empty($item['menus']) ) $linkAttr = array_merge($linkAttr, [
+						'role'          => 'button',
+						'data-toggle'   => 'dropdown',
+						'aria-haspopup' => 'true',
+						'aria-expanded' => 'false',
+					]);
+					// wrap by link (when necessary)
+					if ( !empty($item['url']) or !empty($item['menus']) ) :
+						?><a <?php foreach ( $linkAttr as $key => $val ) echo $key.'="'.$val.'" '; ?>><?php
+					endif;
+					// tab icon
+					if ( !empty($item['icon']) ) :
+						?><i class="<?php echo $item['icon']; ?>"></i> <?php
+					endif;
+					// tab name
+					if ( !empty($item['name']) ) echo $item['name'];
+					// tab remark
+					if ( !empty($item['remark']) ) :
+						$tabRemarkClass = array('ml-1');
+						if ( $tabLayout['style'] == 'tabs' or empty($item['active']) ) $tabRemarkClass[] = 'text-muted';
+						?> <small class="<?php echo implode(' ', $tabRemarkClass); ?>">(<?php echo $item['remark']; ?>)</small><?php
+					endif;
+					// wrap by link (when necessary)
+					if ( !empty($item['url']) or !empty($item['menus']) ) :
+						?></a><?php
+					endif;
+					// dropdown menu (if any)
+					if ( !empty($item['menus']) ) :
+						?><ul class="dropdown-menu"><?php layoutHeaderNav($item['menus'], 2); ?></ul><?php
+					endif;
 					// buttons (for horizontal orientation)
-					if ( !empty($tab['buttons']) and $tabLayout['orientation'] == 'horizontal' ) :
+					if ( !empty($item['buttons']) and $tabLayout['orientation'] == 'horizontal' ) :
 						?><div class="d-inline-block"><?php
 							include F::appPath('view/tab/layout.nav.button.php');
 						?></div><?php
