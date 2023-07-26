afedef93b231f8d75e3cf0756d57f96fe49ccb26
henrygotmojo <henrygotmojo@gmail.com>
2023-07-26 15:30:15 +0800

    apply modal-dialog-scrollable

--- app/view/global/layout.modal.php
+++ app/view/global/layout.modal.php
@@ -8,7 +8,7 @@
 */
 foreach ( ['max','xxl','xl','lg','md','','sm','xs'] as $size ) :
 	?><div id="global-modal<?php if ( !empty($size) ) echo '-'.$size; ?>" class="modal fade" data-backdrop="true" tabindex="-1" role="dialog" aria-hidden="true"><?php
-		?><div class="modal-dialog <?php if ( !empty($size) ) echo 'modal-'.$size; ?>"><?php
+		?><div class="modal-dialog modal-dialog-scrollable <?php if ( !empty($size) ) echo 'modal-'.$size; ?>"><?php
 			?><div class="modal-content"></div><?php
 		?></div><?php
 	?></div><?php
