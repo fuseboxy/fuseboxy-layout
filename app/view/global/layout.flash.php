2149ac1d0659fde8971445f14cd1d0e9ccb0f961
henrygotmojo <henrygotmojo@gmail.com>
2023-08-24 10:22:51 +0800

    apply F::alert() for layout-flash

--- app/view/global/layout.flash.php
+++ app/view/global/layout.flash.php
@@ -2,13 +2,13 @@
 <fusedoc>
 	<io>
 		<in>
-			<string name="flash" scope="$arguments|$_SESSION" />
-			<structure name="flash" scope="$arguments|$_SESSION">
+			<string_or_structure name="flash" scope="$arguments|$_SESSION">
 				<string name="type" optional="yes" default="primary" comments="primary|secondary|success|info|warning|danger|light|dark" />
 				<string name="icon" optional="yes" />
 				<string name="heading" optional="yes" />
 				<string name="message" optional="yes" />
-			</structure>
+				<string name="remark" optional="yes" />
+			</string_or_structure>
 		</in>
 		<out />
 	</io>
@@ -19,22 +19,7 @@ if ( isset($_SESSION['flash']) ) :
 	$arguments['flash'] = $_SESSION['flash'];
 	unset($_SESSION['flash']);
 endif;
-// default
-if ( !empty($arguments['flash']) ) :
-	if ( !is_array($arguments['flash']) ) $arguments['flash'] = array('message' => $arguments['flash']);
-	if ( empty($arguments['flash']['type']) ) $arguments['flash']['type'] = 'primary';
-endif;
 // display (when necessary)
 if ( !empty($arguments['flash']) ) :
-	?><div id="flash" class="alert alert-<?php echo $arguments['flash']['type']; ?>"><?php
-		if ( !empty($arguments['flash']['icon']) ) :
-			?><span class="mr-1"><i class="<?php echo $arguments['flash']['icon']; ?>"></i></span><?php
-		endif;
-		if ( !empty($arguments['flash']['heading']) ) :
-			?><span class="mr-1"><strong><?php echo $arguments['flash']['heading']; ?></strong></span><?php
-		endif;
-		if ( !empty($arguments['flash']['message']) ) :
-			?><span><?php echo $arguments['flash']['message']; ?></span><?php
-		endif;
-	?></div><?php
+	F::alert(array_merge($arguments['flash'], [ 'id' => 'flash' ]));
 endif;
